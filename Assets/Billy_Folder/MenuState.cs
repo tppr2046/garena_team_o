825068b13c03f1d65bbc5db75468cab7531d56a2
playbilly <playbilly358@gmail.com>
2024-05-18 11:23:27 +0800

    場景狀態機

--- Assets/Billy_Folder/MenuState.cs
+++ Assets/Billy_Folder/MenuState.cs
@@ -18,10 +18,14 @@ public class MenuState : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        for (int i = 0; i < animator.parameterCount; i++)
+        {
+            animator.ResetTrigger(i);
+        }
+        
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
