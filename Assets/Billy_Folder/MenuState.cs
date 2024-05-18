81a1857b3906549db93d416b3721d4ad720d7752
playbilly <playbilly358@gmail.com>
2024-05-18 13:15:09 +0800

    新增中文字集

--- Assets/Billy_Folder/MenuState.cs
+++ Assets/Billy_Folder/MenuState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuState : StateMachineBehaviour
+public class SceneFSMController : StateMachineBehaviour
 {
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
