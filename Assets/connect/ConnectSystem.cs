1c4090cda1c8673b759de3a552805b406395bba3
TachibanaNoSuke <TachibanaNoSuke@gmail.com>
2024-05-18 11:21:48 +0800

    [功能] 串接陣列序列化物件並調整部份參數

--- Assets/connect/ConnectSystem.cs
+++ Assets/connect/ConnectSystem.cs
@@ -11,33 +11,39 @@ public class ConnectSystem : MonoBehaviour
     // http 連線物件
     private HttpClient client = new HttpClient();
     // 連線網址(儲存)
-    private readonly string url_save = "http://127.0.0.1:3000/save";
+    private readonly string url_upload = "http://127.0.0.1:3000/upload";
     // 連線網址(讀取)
-    private readonly string url_load = "http://127.0.0.1:3000/load";
+    private readonly string url_download = "http://127.0.0.1:3000/download";
+    // 要儲存的物件
+    public GameObject gameObject;
 
-    public void Save(GameObject gameObject)
+    public void Upload(GameObject gameObject)
     {
-        string str = SerializeGameObject(gameObject);
-        SendDataToServer(str);
+        Debug.Log("Upload GameObject");
+        string str = serializeGameObject(gameObject);
+        sendDataToServer(str);
     }
 
-    private string SerializeGameObject(GameObject gameObject)
+    private string serializeGameObject(GameObject gameObject)
     {
-        SerializableGameObject serializableGameObject = new SerializableGameObject(gameObject);
-        return JsonUtility.ToJson(serializableGameObject);
+        SerializableGameObjectArray serializableGameObjectArray = new SerializableGameObjectArray(gameObject);
+        string str = JsonUtility.ToJson(serializableGameObjectArray);
+        Debug.Log("serializeGameObject str:" + str);
+        return str;
     }
 
 
-    private async Task<string> SendDataToServer(string data)
+    private async Task<string> sendDataToServer(string data)
     {
         try
         {
             // 發送 POST 請求
-            HttpResponseMessage response = await client.PostAsync(url_save, new StringContent(data));
+            HttpResponseMessage response = await client.PostAsync(url_upload, new StringContent(data));
             // 確認是否成功
             response.EnsureSuccessStatusCode();
             // 讀取回應內容
             string responseBody = await response.Content.ReadAsStringAsync();
+            Debug.Log("sendDataToServer responseBody:" + responseBody);
             // 回傳回應內容
             return responseBody;
         }
@@ -48,25 +54,27 @@ public class ConnectSystem : MonoBehaviour
         }
     }
 
-    public async Task<SerializableGameObject> load()
+    public async Task<SerializableGameObjectArray> Download()
     {
-        string response = await GetDataFromServer();
-        Debug.Log("response" + response);
-        SerializableGameObject serializableGameObject = JsonUtility.FromJson<SerializableGameObject>(response);
-        return serializableGameObject;
+        Debug.Log("Download GameObject");
+        // 從 server 端取得資料
+        string response = await getDataFromServer();
+        // 將資料轉換成物件並回傳
+        return JsonUtility.FromJson<SerializableGameObjectArray>(response);
     }
 
     // 從 server 端取得資料
-    private async Task<string> GetDataFromServer()
+    private async Task<string> getDataFromServer()
     {
         try
         {
             // 發送 GET 請求
-            HttpResponseMessage response = await client.GetAsync(url_load);
+            HttpResponseMessage response = await client.GetAsync(url_download);
             // 確認是否成功
             response.EnsureSuccessStatusCode();
             // 讀取回應內容
             string responseBody = await response.Content.ReadAsStringAsync();
+            Debug.Log("getDataFromServer responseBody:" + responseBody);
             // 回傳回應內容
             return responseBody;
         }
@@ -75,6 +83,32 @@ public class ConnectSystem : MonoBehaviour
             Debug.LogError($"Request failed: {e.Message}");
             return null;
         }
+    }
+
+    // 用來將物件實例化的範例
+    private void createGameObject(SerializableGameObject serializableGameObject)
+    {
+        GameObject newGameObject;
+        // 依照名稱來決定要實例化的物件
+        if (serializableGameObject.name == "Cube")
+        {
+            newGameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        }
+        else if (serializableGameObject.name == "Sphere")
+        {
+            newGameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        }
+        else if (serializableGameObject.name == "Capsule")
+        {
+            newGameObject = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        }
+        else
+        {
+            newGameObject = new GameObject();
+        }
+        // 設定物件的位置與旋轉
+        newGameObject.transform.position = serializableGameObject.position;
+        newGameObject.transform.rotation = serializableGameObject.rotation;
     }
 
 }
