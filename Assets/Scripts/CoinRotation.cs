2e219a6ea7fc0bf2e113f30d31ca33a0495260a2
playbilly <playbilly358@gmail.com>
2024-05-19 13:18:55 +0800

    起始按鈕特效功能

--- Assets/Scripts/CoinRotation.cs
+++ Assets/Scripts/CoinRotation.cs
@@ -6,10 +6,21 @@ public class CoinRotation : MonoBehaviour
 {
 
     public float rotationSpeed = 0.1f;
+    public GameObject expoled;
 
     // Update is called once per frame
     void FixedUpdate()
     {
         this.transform.Rotate(0, rotationSpeed, 0);
+    }
+    public void SetSpeed(float speed)
+    {
+        rotationSpeed = speed;
+    }
+    public void TriggerStart(float speed)
+    {
+        //¼Q¯S®Ä
+        expoled.SetActive(true);
+        SetSpeed(speed);
     }
 }
